8f97a5fd735573371583d32b792d11fb3511beb7
Davi Severino <daviseverinoinf2@gmail.com>
2026-03-18 13:55:05 -0300

    .

--- site/script.js
+++ site/script.js
@@ -9,6 +9,18 @@ document.querySelectorAll(".metais_alcalinos_terrosos").forEach(el => {
 document.querySelectorAll(".metais_de_transição").forEach(el => {
     el.style.background = "#6A5ACD"
 })
+
+document.querySelectorAll(".metais_pós_transição").forEach(el => {
+    el.style.background = "#7FFFD4"
+})
+
+document.querySelectorAll(".semi_metais").forEach(el => {
+    el.style.background = "#B0E0E6"
+})
+
+document.querySelectorAll(".semi_metais").forEach(el => {
+    el.style.background = "#B0E0E6"
+})
 let botao = document.querySelector("#buttonMenu")
 let menu = document.querySelector("#menuDropdown")
 
