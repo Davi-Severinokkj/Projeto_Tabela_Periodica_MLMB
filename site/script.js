339d8af9ec8bcfb68d048aca2406ac37fa44d5ea
Davi Severino <daviseverinoinf2@gmail.com>
2026-03-18 13:39:19 -0300

    .

--- site/script.js
+++ site/script.js
@@ -1,9 +1,26 @@
+document.querySelectorAll(".metais_alcalinos").forEach(el => {
+    el.style.background = "#008080"
+})
+
+document.querySelectorAll(".metais_alcalinos_terrosos").forEach(el => {
+    el.style.background = "#00FFFF"
+})
+
+document.querySelectorAll(".metais_de_transição").forEach(el => {
+    el.style.background = "#6A5ACD"
+})
 let botao = document.querySelector("#buttonMenu")
 let menu = document.querySelector("#menuDropdown")
 
 botao.addEventListener("click", function(){
     menu.classList.toggle("ativo")
 })
+
+window.onload = () => {
+    document.getElementById("modal").style.display = "none"
+}
+
+
 
 let elemento = document.querySelector(".elemento")
 elemento.addEventListener('click', abrirModal())
