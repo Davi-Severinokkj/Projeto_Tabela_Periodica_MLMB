3041f983add520d67438fa6e2849d0cdd853574d
Davi Severino <daviseverinoinf2@gmail.com>
2026-03-18 12:10:49 -0300

    .

--- site/script.js
+++ site/script.js
@@ -5,14 +5,15 @@ botao.addEventListener("click", function(){
     menu.classList.toggle("ativo")
 })
 
-let elemento = document.querySelector(".simbolo")
-elemento.addEventListener('click' , informacao())
+let elemento = document.querySelector(".elemento")
+elemento.addEventListener('click', abrirModal())
 
-function informacao(){
-
+function abrirModal(nome, descricao){
+    document.getElementById("modal").style.display = "block"
+    document.getElementById("titulo").innerText = nome
+    document.getElementById("descricao").innerText = descricao
 }
 
-let imagem_elemento = document.querySelector("#image_element")
-let informacao_elemento = document.querySelector(".info_element")
-let button_about_more = document.querySelector("#abou_more_element")
-
+function fecharModal(){
+    document.getElementById("modal").style.display = "none"
+}
