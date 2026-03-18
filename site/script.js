7b33b1125b52f423b9786a3fb4020261d56a60e5
Davi Severino <daviseverinoinf2@gmail.com>
2026-03-18 11:49:56 -0300

    .

--- site/script.js
+++ site/script.js
@@ -7,4 +7,4 @@ botao.addEventListener("click", function(){
 
 let imagem_elemento = document.querySelector("#image_element")
 let informacao_elemento = document.querySelector(".info_element")
-let 
+let button_about_more = document.querySelector("#abou_more_element")
