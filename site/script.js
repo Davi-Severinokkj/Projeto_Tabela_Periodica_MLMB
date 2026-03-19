dc817d168d9aa71419531b8aee0ce7cae3220c4e
Davi Severino Oliveira de Souza <daviseverinoinf2@gmail.com>
2026-03-19 20:20:35 -0300

    .

--- site/script.js
+++ site/script.js
@@ -1,27 +1,3 @@
-// ===== CORES POR CLASSE =====
-
-const cores = {
-    metais_alcalinos: "#008080",
-    metais_alcalinos_terrosos: "#00FFFF",
-    metais_de_transicao: "#6A5ACD",
-    metais_pos_transicao: "#7FFFD4",
-    semi_metais: "#B0E0E6",
-    nao_metais_poliatomicos: "#87CEFA",
-    nao_metais_diatomicos: "#5F9EA0",
-    gases_nobres: "#BA55D3",
-    desconhecidos: "#4B0082",
-    lantanideos: "#C71585",
-    actinideos: "#FF1493"
-}
-
-// aplica cores automaticamente
-Object.keys(cores).forEach(classe => {
-    document.querySelectorAll(`.${classe}`).forEach(el => {
-        el.style.background = cores[classe]
-    })
-})
-
-
 // ===== MENU =====
 let botao = document.querySelector("#buttonMenu")
 let menu = document.querySelector("#menuDropdown")
