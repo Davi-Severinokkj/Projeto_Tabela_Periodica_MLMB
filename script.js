ba69af017be70ba65d906bdc2b940b4fd19d3d88
Davi Severino Oliveira de Souza <daviseverinoinf2@gmail.com>
2026-03-25 00:03:04 -0300

    .

--- script.js
+++ script.js
@@ -2,21 +2,48 @@
 let botao = document.querySelector("#buttonMenu")
 let menu = document.querySelector("#menuDropdown")
 
+
+
 botao.addEventListener("click", () => {
     menu.classList.toggle("ativo")
+
+    if(menu.classList.contains("ativo")){
+        document.body.style.overflow = "hidden"
+    } else {
+        document.body.style.overflow = "auto"
+    }
+})
+
+
+let tabelaPeriodica = document.querySelector(".table-periodic")
+let botaoTabela = document.querySelector("#buttonTableP")
+
+botaoTabela.addEventListener("click", ()=>{
+    tabelaPeriodica.classList.toggle('form')
 })
 
 
 // ===== MODAL =====
+
 const modal = document.getElementById("modal")
-const titulo = document.getElementById("titulo")
+const imagem = document.getElementById("imagem_elemento")
+const nome = document.getElementById("nome")
+const simbolo = document.getElementById("simbolo_modal")
+const numberAtomic = document.getElementById("numero_atomico")
+const massAtomic = document.getElementById("massa_atomica")
 const descricao = document.getElementById("descricao")
+
 
 // adiciona evento em TODOS os elementos
 document.querySelectorAll(".elemento").forEach(el => {
     el.addEventListener("click", () => {
-        titulo.innerText = el.dataset.nome
-        descricao.innerText = el.dataset.desc
+        document.body.style.overflow = 'hidden'
+        imagem.src = el.dataset.imagem
+        nome.innerText = el.dataset.nome
+        simbolo.innerHTML = el.dataset.simbolo
+        numberAtomic.innerHTML = el.dataset.numberAtomic
+        massAtomic.innerHTML = el.dataset.massAtomic
+        descricao.innerText = el.dataset.descricao
         modal.style.display = "block"
     })
 })
@@ -43,9 +70,12 @@ window.onload = () => {
 let links = document.querySelectorAll("#menuDropdown a")
 let ativo = null
 
+let voltar = document.getElementsByTagName('body')
 links.forEach(link => {
     link.addEventListener("click", (e) => {
         e.preventDefault()
+
+        
 
         let classe = link.getAttribute("data-classe")
 
@@ -54,6 +84,8 @@ links.forEach(link => {
                 el.classList.remove("aaaa")
             })
             ativo = null
+
+            document.body.style.overflow = "auto" // 👈 AQUI
             return
         }
 
@@ -66,5 +98,9 @@ links.forEach(link => {
         document.querySelectorAll("." + classe).forEach(el => {
             el.classList.add("aaaa")
         })
+
+        document.body.style.overflow = "auto" // 👈 E AQUI
+        document.body.style.overflow = "auto"
+        menu.classList.remove("ativo")
     })
 })
